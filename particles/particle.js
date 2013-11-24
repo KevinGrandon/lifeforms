ff82ba1751f0d2d217984c1a725513cdac52b54d
Kevin Grandon <kevingrandon@yahoo.com>
2013-11-24 13:35:49 -0800

    wip

--- particles/particle.js
+++ particles/particle.js
@@ -1,9 +1,19 @@
 function Particle(x, y, z) {
 	z = z || 0
 	this.location = [x, y, z]
+	this.health = 100
 }
 
 Particle.prototype = {
+
+	/**
+	 * Gets a single particle in an offset direction
+	 * @param {String} offset axis. One of x|y|z
+	 * @param {Integer} amount of offset. -1 or 1
+	 */
+	getOffsetParticle: function(axis, amount) {
+
+	},
 
 	/**
 	 * Called on every tick
